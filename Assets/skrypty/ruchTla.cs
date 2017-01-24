7ef84fbea37099e676d2f1a219e3bd1b93f705f7
minesotha <minesotha@gmail.com>
2017-01-25 00:14:02 +0100

    uporządkowanie elementów scen

--- Assets/skrypty/ruchTla.cs
+++ Assets/skrypty/ruchTla.cs
@@ -2,23 +2,19 @@
 using System.Collections;
 
 public class ruchTla : MonoBehaviour {
-	
-	public Transform ksiezniczka;
-	public float tlo;
+		public float tlo;
 
+    //kamera porusza się jednostajnie
 	void RuchKamery(){
 		
 		Vector3 wektor = new Vector3 (tlo*Time.deltaTime, 0, 0);
 		transform.Translate (wektor);
 		
 	}
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {RuchKamery ();
+
+
+	void FixedUpdate () {
+        RuchKamery ();
 		
 	}
 }
